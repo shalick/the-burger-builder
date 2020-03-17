e7e35e9d7a0904b0fe6ac3f01f93528d51423da4
shalick <alikshabanovich@gmail.com>
2020-03-18 01:09:19 +0300

    Updates

---
+++ precache-manifest.62c8eebf4cc4e1c64d907d1c19321962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7ee9d156fbe1176d4f82c7eec9d0ca",
+    "url": "/shalick/the-burger-builder/index.html"
+  },
+  {
+    "revision": "7895a05ef8e116416a7f",
+    "url": "/shalick/the-burger-builder/static/css/main.66b3abde.chunk.css"
+  },
+  {
+    "revision": "e32453538d9b46bf6084",
+    "url": "/shalick/the-burger-builder/static/js/2.33dd830b.chunk.js"
+  },
+  {
+    "revision": "65f61a1b574cbfdae1aa1e0f7a13f7ca",
+    "url": "/shalick/the-burger-builder/static/js/2.33dd830b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7895a05ef8e116416a7f",
+    "url": "/shalick/the-burger-builder/static/js/main.33d28fad.chunk.js"
+  },
+  {
+    "revision": "444f82659cc8902e8060",
+    "url": "/shalick/the-burger-builder/static/js/runtime-main.d8bdd3a8.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/shalick/the-burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
